bcc49800e421d11b9d62c5e7c958cba9d59d7176
amshirazi64 <amshirazi64@gmail.com>
2023-07-20 11:25:37 +0330

    step 2
    admin page,add sum book,book detail view,book list view

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', views.index, name='index' ),
+    path('', views.Index.as_view(), name='index' ),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]

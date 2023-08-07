6915d2df71c1998edec9d6a36ca042f5155f339e
amshirazi64 <amshirazi64@gmail.com>
2023-08-07 15:52:55 +0330

    update

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!4dk4xo0#c*f00802bq$l4)&7ob0t*d&v58rli=xwrh86gyq$@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
